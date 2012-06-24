365925a0c7dfdfe2e63e4a6edb07246c66fe2d6f
Mathias Rav <rav@cs.au.dk>
2012-06-24 09:02:48 +0200

    Progress

--- test.js
+++ test.js
@@ -4,28 +4,30 @@ function expectbuffer(str, buf) {
   return v.getBuffer() == buf;
 }
 function vimescape(s) {
-  return s.replace(/\x1b/g, '<Esc>');
+  return s.replace(/\x1b/g, '<Esc>')
+    .replace(/\n/g, '<CR>');
 }
-function runtests() {
-  var res = document.createElement('div');
-  res.id = 'testresults';
-  document.body.appendChild(res);
-  var tests = arguments;
-  for (var i = 0, l = tests.length; i < l; ++i) {
-    var line = document.createElement('div');
-    line.className = 'line';
-    line.appendChild(document.createTextNode(vimescape(tests[i][0])));
-    res.appendChild(line);
-    if (expectbuffer(tests[i][0], tests[i][1])) {
-      line.className += ' good';
-    } else {
-      line.className += ' bad';
-    }
+var res;
+function t(type, expect) {
+  var line = document.createElement('div');
+  line.className = 'line';
+  line.appendChild(document.createTextNode(vimescape(type)));
+  res.appendChild(line);
+  if (expectbuffer(type, expect)) {
+    line.className += ' good';
+  } else {
+    line.className += ' bad';
   }
 }
 window.onload = function () {
-  runtests(
-    ['ihej\x1b', 'hej\n']
-  );
+  res = document.createElement('div');
+  res.id = 'testresults';
+  document.body.appendChild(res);
+  t('ihej\x1b', 'hej\n');
+  t('Ahej\x1b', 'hej\n');
+  t('Chej\x1b', 'hej\n');
+  t('Shej\x1b', 'hej\n');
+  t('ihej\x1b0Difarvel\x1b', 'farvel\n');
+  t('ihej\nhej\x1b0Difarvel\x1b', 'hej\nfarvel\n');
 };
 // vim:set sw=2 sts=2 et:
--- vim.js
+++ vim.js
@@ -4,11 +4,15 @@ Mode.NORMAL = new Mode();
 Mode.INSERT = new Mode();
 function Vim() {
   this.mode = Mode.NORMAL;
-  this.lines = [''];
-  this.curline = 0;
+  this.buffer = '\n';
+  this.cursor = 0;
 }
+Vim.prototype.changeText = function (i, j, s) {
+  this.buffer = this.buffer.substring(0, i) + s + this.buffer.substring(j, this.buffer.length);
+};
 Vim.prototype.addText = function (c) {
-  this.lines[this.curline] += c;
+  this.changeText(this.cursor, this.cursor, c);
+  this.cursor += c.length;
 };
 Vim.prototype.input = function (str) {
   for (var i = 0, l = str.length; i < l;) {
@@ -20,8 +24,19 @@ Vim.prototype.input = function (str) {
       case Mode.NORMAL:
         switch (c) {
           case 'i':
+          case 'A':
+          case 'C':
+          case 'S':
             this.mode = Mode.INSERT;
             break;
+          case '0':
+            while (this.cursor && this.buffer.charAt(this.cursor-1) != '\n')
+              --this.cursor;
+          case 'D':
+            var j = this.cursor;
+            while (j < this.buffer.length && this.buffer.charAt(j) != '\n')
+              ++j;
+            this.changeText(this.cursor, j, '');
         }
         break;
       case Mode.INSERT:
@@ -35,6 +50,6 @@ Vim.prototype.input = function (str) {
   }
 };
 Vim.prototype.getBuffer = function () {
-  return this.lines.join('\n')+'\n';
+  return this.buffer;
 };
 // vim:set sw=2 sts=2 et:
