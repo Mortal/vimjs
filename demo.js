1c3ed158d60515c7c3b4c64be9db5682fc4ee74f
Mathias Rav <rav@cs.au.dk>
2012-07-25 23:35:37 +0200

    demo.js: Refactor input handling, accept keypress events

--- demo.js
+++ demo.js
@@ -1,34 +1,53 @@
-// Input mapping
-var keyMap = {
-  8:   '\b',
-  13:  '\n',
-  46:  '\x7f', // Delete
-  188: ',',
-  190: '.'
+function keydebug() {
+  return ('undefined' != typeof console)
+    ? console.log.apply(console, arguments)
+    : null;
 }
-function keyDownToChar(ev) { // This function is going to be quite the beast.
+function translate_key_down(ev) { // This function is going to be quite the beast.
   var kc = ev.keyCode;
-  if ((   kc != 27 // Ignore control-characters
-       && kc != 13
-       && kc != 8
-       && kc < 32)
-      || (kc > 111 && kc < 124) // Ignore F-keys
-      ) return '';
-  var k;
+  keydebug("Key code ",kc);
+  // Input mapping
+  var keyMap = {
+    8:   '\b',
+    13:  '\n',
+    27:  '\x1b', // escape
+    46:  '\x7f', // Delete
+    188: ',',
+    190: '.'
+  };
 
-  var mapFound = false;
-  for(var code in keyMap)
-    if(kc == code) mapFound = true;
+  if ('undefined' != typeof keyMap[kc])
+    return keyMap[kc];
 
-  if(mapFound) k = keyMap[kc];
-  else if(ev.shiftKey) k = String.fromCharCode(kc);
-  else k = String.fromCharCode(kc).toLowerCase();
+  if (65 <= kc && kc <= 90) {
+    if (ev.ctrlKey)
+      return String.fromCharCode(kc-64);
+    else if (ev.shiftKey)
+      return String.fromCharCode(kc);
+    else
+      return String.fromCharCode(kc+32);
+  }
+  if (219 <= kc && kc <= 221) {
+    if (ev.ctrlKey)
+      return String.fromCharCode(kc-192);
+    else if (ev.shiftKey)
+      return String.fromCharCode(kc-96);
+    else
+      return String.fromCharCode(kc-128);
+  }
 
-  console.log(kc,k,ev);
-  ev.preventDefault();
-  return k;
+  return null;
 }
-
+function translate_key_press(ev) {
+  var cc = ev.charCode;
+  keydebug("Char code ",cc);
+  if (!cc) return null;
+  if (65 <= cc && cc <= 90) return null; // A-Z
+  if (97 <= cc && cc <= 122) return null; // a-z
+  var c = String.fromCharCode(cc);
+  if ('[\\]{|}'.indexOf(c) != -1) return null;
+  return c;
+}
 // Vim-visual stuff
 var vimdiv, errordiv, statuslinediv, vim;
 window.onload = function () {
@@ -43,10 +62,19 @@ function update() {
   vimdiv.textContent = buf;
   statuslinediv.textContent = vim.getStatusline();
 }
-var onKey = function(ev) {
-  var k = keyDownToChar(ev);
+function vimescape(k) {
+  if (k == '\x1b') return '<Esc>';
+  if (k == ' ') return '<SP>';
+  if (k == '\x7f') return '<BS>';
+  if ('string' == typeof k && k.length == 1 && k.charCodeAt(0) < 32) return '0x'+k.charCodeAt(0).toString(16);
+  return k;
+}
+var onKey = function(ev, k) {
   try {
-    vim.input(k);
+    if (vim.input(k)) {
+      ev.preventDefault();
+      ev.stopPropagation();
+    }
   } catch (e) {
     if (e instanceof InfiniteLoop) {
       if (!errordiv) {
@@ -54,11 +82,23 @@ var onKey = function(ev) {
         errordiv.id = 'error';
       }
       errordiv.innerHTML = 'Infinite loop';
+      return;
     } else {
       throw e;
     }
   }
   update();
-}
-document.addEventListener('keydown', onKey, false);
+};
+document.addEventListener('keydown', function (ev) {
+  var k = translate_key_down(ev);
+  if (k == null) return;
+  keydebug("Key down event translated to char ",vimescape(k),ev);
+  return onKey(ev, k);
+}, false);
+document.addEventListener('keypress', function (ev) {
+  var k = translate_key_press(ev);
+  if (k == null) return;
+  keydebug("Key press event translated to char ",vimescape(k));
+  return onKey(ev, k);
+}, false);
 // vim:set sw=2 sts=2 et:
